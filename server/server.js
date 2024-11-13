75dfe40f8e8b82a42ea7a510855aa58a04065f6e
manish1034 <manishchandra1034@gmail.com>
2024-11-13 16:01:33 +0530

    my first commit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require('dotenv').config();
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(()=> console.log("MongoDB Connected"))
+    .catch((err)=> console.log(err));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma",
+            "Set-Cookie",
+            "X-CSRF-TOKEN",
+            "X-Requested-With",
+        ],
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
