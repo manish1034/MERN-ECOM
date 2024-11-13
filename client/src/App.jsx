8da4c48ebda5abd9526c734a23e291121d8e3ac4
manish1034 <manishchandra1034@gmail.com>
2024-11-13 16:53:41 +0530

    made new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,40 @@ import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminFeatures from "./pages/admin-view/features";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
+import ShoppingLayout from "./components/shopping-view/layout";
+import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingListing from "./pages/shopping-view/listing";
+import ShoppingAccount from "./pages/shopping-view/account";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
 import './index.css'
 
 function App() {
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      <h1>Hello</h1>
-
       <Routes>
         <Route path="/auth" element={<AuthLayout/>}>
           <Route path="login" element={<AuthLogin/>}/>
           <Route path="register" element={<AuthRegister/>}/>
         </Route>
+        <Route path="/admin" element={<AdminLayout/>}>
+          <Route path="dashboard" element={<AdminDashboard/>}/>
+          <Route path="features" element={<AdminFeatures/>}/>
+          <Route path="products" element={<AdminProducts/>}/>
+          <Route path="orders" element={<AdminOrders/>}/>
+        </Route>
+        <Route path="/shop" element={<ShoppingLayout/>}>
+          <Route path="home" element={<ShoppingHome/>}/>
+          <Route path="listing" element={<ShoppingListing/>}/>
+          <Route path="account" element={<ShoppingAccount/>}/>
+          <Route path="checkout" element={<ShoppingCheckout/>}/>
+        </Route>
+        <Route path="*" />
       </Routes>
     </div>
   )
---
+++ client/src/components/shopping-view/layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import ShoppingHeader from "./header";
+
+function ShoppingLayout() {
+    return (
+        <div className="flex flex-col overflow-hidden bg-white">
+            {/* Common header  */}
+            <ShoppingHeader />
+            <main className="flex flex-col w-full">
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default ShoppingLayout;
